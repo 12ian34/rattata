a777eb52b1bb16b67defc5628bf4ec4b73fc4f96
Ian Ahuja <12ian34@gmail.com>
2019-12-28 19:17:43 -0500

    first draft of loop complete for testing

--- ra-ticket-resale-watch.py
+++ ra-ticket-resale-watch.py
@@ -1,131 +1,92 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# In[516]:
+# In[56]:
 
 
-# import necessary packages
 import lxml
+import os
 import requests as re
+import time
 from lxml import html
 from tabulate import tabulate
+from datetime import datetime
+from twilio.rest import Client
 
 
-# In[518]:
+# In[57]:
 
 
-# ask for full ra url
+account_sid = os.environ["TWILIO_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+twilio_number = os.environ["TWILIO_NUMBER"]
+my_number = os.environ["MY_NUMBER"]
+
+
+# In[20]:
+
+
 raurl = input("\nfull Resident Advisor URL \n")
 
-# confirm url
-print("\nchecking tickets for\n" + raurl + "\n")
+
+# In[51]:
 
 
-# In[519]:
+def check_tickets():
+    print("date:")
+    print(datetime.now())
+    print("\nchecking tickets for:\n" + raurl + "\n")
 
+    print("sending http request:")
+    response = re.get(raurl)
+    print(response)
 
-# send HTTP request and store response
-print("sending http request\n")
-response = re.get(raurl)
-print(response)
+    tree = lxml.html.fromstring(response.text)
+    ticket_classes = tree.xpath('//*[@id="tickets"]/ul/li/@class')
+    offsale_types = tree.xpath('//*[@id="tickets"]/ul/li/p/text()')
+    offsale_prices = tree.xpath('//*[@id="tickets"]/ul/li/p/span/text()')
+    onsale_types = tree.xpath('//*[@id="tickets"]/ul/li/label/p/text()')
+    onsale_prices = tree.xpath('//*[@id="tickets"]/ul/li/label/p/span/text()')
+    event_title = tree.xpath('//div/h1')[0].text
+    event_day = tree.xpath('//main/ul/li/section/div/div/div/aside/ul/li/text()')[0]
+    event_date = tree.xpath('//main/ul/li/section/div/div/div/aside/ul/li/a/text()')[0]
+    event_time = tree.xpath('//main/ul/li/section/div/div/div/aside/ul/li/text()')[1]
+    event_location = tree.xpath('//*[@id="detail"]/ul/li[2]/a[1]/text()')[0]
 
+    print("\nevent title: \n" + event_title)
+    print("\ndate: \n" + event_day + "\n" + event_date + "\n" + event_time)
+    print("\nlocation:\n" + event_location)
+    print(f"There are {len(offsale_types)} ticket types off sale")
+    print(f"There are {len(onsale_types)} ticket types on sale")
+    
+    print("\nthe following tickets are off sale: \n")
+    headers = ['ticket', 'price']
+    table = zip(offsale_types, offsale_prices)
+    print(tabulate(table, headers=headers, floatfmt=".4f"))
 
-# In[520]:
-
-
-# parse xml tree
-tree = lxml.html.fromstring(response.text)
-
-
-# In[521]:
-
-
-# extract and print event title
-event_title = tree.xpath('//div/h1')[0].text
-print("\nevent title: \n" + event_title)
-
-
-# In[522]:
-
-
-# extract and print event date
-event_day = tree.xpath('//main/ul/li/section/div/div/div/aside/ul/li/text()')[0]
-event_date = tree.xpath('//main/ul/li/section/div/div/div/aside/ul/li/a/text()')[0]
-event_time = tree.xpath('//main/ul/li/section/div/div/div/aside/ul/li/text()')[1]
-print("\ndate: \n" + event_day + "\n" + event_date + "\n" + event_time)
-
-
-# In[523]:
-
-
-# extract and print event location
-event_location = tree.xpath('//*[@id="detail"]/ul/li[2]/a[1]/text()')[0]
-print("\nlocation:\n" + event_location)
-
-
-# In[524]:
-
-
-# extract ticket class list, not used yet
-ticket_classes = tree.xpath('//*[@id="tickets"]/ul/li/@class')
-
-
-# In[525]:
-
-
-# extract and print off sale ticket detail lists 
-offsale_types = tree.xpath('//*[@id="tickets"]/ul/li/p/text()')
-offsale_prices = tree.xpath('//*[@id="tickets"]/ul/li/p/span/text()')
-
-
-# In[526]:
-
-
-print("\nthe following tickets are off sale: \n")
-headers = ['ticket', 'price']
-table = zip(offsale_types, offsale_prices)
-print(tabulate(table, headers=headers, floatfmt=".4f"))
-print("\n ... one day ... \n")
-
-
-# In[527]:
-
-
-# extract and print on sale ticket detail lists 
-onsale_types = tree.xpath('//*[@id="tickets"]/ul/li/label/p/text()')
-onsale_prices = tree.xpath('//*[@id="tickets"]/ul/li/label/p/span/text()')
-
-
-# In[529]:
-
-
-# inform whether tickets are on sale, if so, print table and share link
-
-if len(onsale_types)==0:
-    print("\nnothing on sale:(\n")
-else:
-    print("the following tickets are on sale: \n")
+    print("\n\nthe following tickets are on sale: \n")
     headers = ['ticket', 'price']
     table = zip(onsale_types, onsale_prices)
-    print(tabulate(table, headers=headers, floatfmt=".4f"))
-    print("\ngo buy them quick\n" + raurl)
+    print(tabulate(table, headers=headers, floatfmt=".4f") + "\n")
+    
+    if len(onsale_types) > 0:
+    print("\ngo buy them quick\n" + raurl + "\n")
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body = "ticket available: " + raurl,
+        from_= twilio_number,
+        to = my_number
+    )
+    print(message.sid)
+            
+    elif len(onsale_types) == 0:
+        print("nothing on sale. retrying in 2 min")
 
 
-# # old code
-
-# In[530]:
+# In[ ]:
 
 
-#for idx, val in enumerate(ticket_classes):
-#    print(idx, val)
-
-
-# In[531]:
-
-
-#for item in tree.xpath("//li"):
-#    if "onsale but" in item.classes:
-#        print("on sale")
-#    else:
-#        print("closed :(")
+while True:
+  count_tickets()
+  time.sleep(300)
 
